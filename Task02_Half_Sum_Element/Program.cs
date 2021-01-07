f8c04d14536bc27c860aa567f698099def22a8ff
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:55:52 +0200

    Add files via upload

---
+++ Task02_Half_Sum_Element/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task02_Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int nextDigit = int.Parse(Console.ReadLine());
+                sum = sum + nextDigit;
+                if (max <nextDigit)
+                {
+                    max = nextDigit;
+                }
+            }
+            if (sum - max == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum - max - max)}");
+            }
+        }
+    }
+}
