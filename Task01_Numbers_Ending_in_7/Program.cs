d66783c7ddb11792545010d695cf9e1f073a9a8a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:54:41 +0200

    Add files via upload

---
+++ Task01_Numbers_Ending_in_7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exersice04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 997; i+=10)
+            {
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
