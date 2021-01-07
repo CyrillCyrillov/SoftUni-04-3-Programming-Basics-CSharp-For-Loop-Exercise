1b46cc9aafdd747e2587db48dff5717a62f14f96
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 19:58:48 +0200

    Add files via upload

---
+++ Task05_Divide_Without_Remainder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task05_Divide_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int nextNumber = int.Parse(Console.ReadLine());
+                if (nextNumber % 2 == 0)
+                {
+                    p1 += 1;
+                }
+                if (nextNumber % 3 == 0)
+                {
+                    p2 += 1;
+                }
+                if (nextNumber % 4 == 0)
+                {
+                    p3 += 1;
+                }
+            }
+            Console.WriteLine($"{(p1 / n * 100):F2}%");
+            Console.WriteLine($"{(p2 / n * 100):F2}%");
+            Console.WriteLine($"{(p3 / n * 100):F2}%");
+        }
+    }
+}
